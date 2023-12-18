296cf333e4d57274b2bb488273c8fff700adefa2
Naman Jolly <naman.jolly2003@gmail.com>
2023-12-18 16:14:13 +0530

    Working SVM Model

--- record.py
+++ record.py
@@ -30,23 +30,26 @@ def face_record():
 	iter1=0
 	while(iter1<num_of_sample):
 		r,frame = vid.read();# capture a single frame
+		if frame is None:
+			print("Error: Unable to capture frame.")
+			break
 		frame = cv2.resize(frame,(640,480)) # resizig the frame
 		im1 = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)# gray scale conversion of
 		# color image
 		face=face_cascade.detectMultiScale(im1,1.3,5)
 		for x,y,w,h in (face):
 		    # [255,0,0] #[B,G,R] 0 to 255 
-		    cv2.rectangle(frame,(x,y),(x+w,y+h),[0,0,255],4)
-		    iter1=iter1+1
-		    im_f = im1[y:y+h,x:x+w]
-		    im_f = cv2.resize(im_f,(112,92))#orl face matching size
-		    cv2.putText(frame,'sample no.'+str(iter1),(x,y), cv2.FONT_ITALIC, 1,
-		               (255,0,255),2,cv2.LINE_AA)
-		    path2 = './recorded_images/{}/{}.png'.format(name,iter1) # path to save the image
-		    cv2.imwrite(path2,im_f) # to save the image 
+			cv2.rectangle(frame,(x,y),(x+w,y+h),[0,0,255],4)
+			iter1=iter1+1
+			im_f = im1[y:y+h,x:x+w]
+			im_f = cv2.resize(im_f,(112,92))#orl face matching size
+			cv2.putText(frame,'sample no.'+str(iter1),(x,y), cv2.FONT_ITALIC, 1,
+									(255,0,255),2,cv2.LINE_AA)
+			path2 = './recorded_images/{}/{}.png'.format(name,iter1) # path to save the image
+			cv2.imwrite(path2,im_f) # to save the image 
 		    
 		cv2.imshow('frame',frame)# display
-		cv2.waitKey(1)
+		cv2.waitKey(10)
 	vid.release()
 	cv2.destroyAllWindows()
 
--- train.py
+++ train.py
@@ -15,7 +15,7 @@ def train_model():
 	a,files,b = next(os.walk("./recorded_images")) #to find out the no: of users
 	train_data=np.zeros((15*len(files),280)) #creating a 2D array of zeroes to store the features of a images later 
 	train_label=np.zeros((15*len(files))) #creating a array for storing labels of the images 
-	print(files)
+	# print(files)
 	for i in files:
 		label = label+1
 		for j in range(1,16): 
@@ -27,19 +27,19 @@ def train_model():
 																		cells_per_block=(1,1))
 			train_data[count,:]=feat.reshape(1,-1) #converting the features in a 1D array and storing it in a 2D array
 			train_label[count]=label #storing the label of the particular user
-			print(i,j,label)
+			# print(i,j,label)
 		df.loc[label]=i
 
 	# model creation
-	# svm_model = svm.SVC(kernel='linear',gamma='scale')
+	svm_model = svm.SVC(kernel='linear',gamma='scale')
 	
 	# # train the model
-	# svm_model = svm_model.fit(train_data,train_label)
-	rf_model = RandomForestClassifier(n_estimators=100)
-	rf_model = rf_model.fit(train_data, train_label)
+	svm_model = svm_model.fit(train_data,train_label)
+	# rf_model = RandomForestClassifier(n_estimators=100)
+	# rf_model = rf_model.fit(train_data, train_label)
 
 
-	joblib.dump(rf_model,'svm_face_train_modelnew.pkl') #saving the created model 
+	joblib.dump(svm_model,'svm_face_train_modelnew.pkl') #saving the created model 
 	print(df)
 	df.to_csv("./target.csv")
 	print('training done ')
